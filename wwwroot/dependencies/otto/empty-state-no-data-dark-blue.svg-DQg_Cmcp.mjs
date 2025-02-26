3d1bef1989727692be10fe713048315859b5584c
Jean-François Morin <2498456+jfmmm@users.noreply.github.com>
2025-02-26 15:02:58 -0500

    beta

---
+++ wwwroot/dependencies/otto/empty-state-no-data-dark-blue.svg-DQg_Cmcp.mjs
@@ -0,0 +1,210 @@
+import * as e from "react";
+var p, o, d, h, i, c, m, v, s, k, u, E, Z, _, f, L, A, B, M, D, V, F, j, R, W, g, w, b, y, O, S, x, N, P, q, z, C, G, H, I, J, K, Q, T;
+function r() {
+  return r = Object.assign ? Object.assign.bind() : function(a) {
+    for (var t = 1; t < arguments.length; t++) {
+      var l = arguments[t];
+      for (var n in l)
+        Object.prototype.hasOwnProperty.call(l, n) && (a[n] = l[n]);
+    }
+    return a;
+  }, r.apply(this, arguments);
+}
+var U = function(t) {
+  return /* @__PURE__ */ e.createElement("svg", r({
+    width: 135,
+    height: 141,
+    fill: "none",
+    xmlns: "http://www.w3.org/2000/svg"
+  }, t), p || (p = /* @__PURE__ */ e.createElement("path", {
+    opacity: 0.3,
+    d: "m43.88 122.27 9.33.39 16 9.57 5.1 8 16.28-1 12.09-7.23 2 1.47a3.261 3.261 0 0 0 3.58.17l14-8.34a.68.68 0 0 0 .08-1.12l-2.34-1.89 14.22-8.51a.23.23 0 0 0 0-.4l-3.54-2.4-18.68 1.74 4.72-9.82 8.55-4.81a.34.34 0 0 0 0-.59l-12.4-7-49.36-1.61-21.86 27.31 2.23 6.07Z",
+    fill: "#1A1F25"
+  })), o || (o = /* @__PURE__ */ e.createElement("path", {
+    d: "m43.47 121.5-41-23.68a1.94 1.94 0 0 1-.75-1.77V29.57a5.25 5.25 0 0 1 2.2-4.2l40.35-23.3A1.41 1.41 0 0 1 45.69 2l41 23.66-30.25 38.2-12.97 57.64Z",
+    fill: "#A2B5DF"
+  })), d || (d = /* @__PURE__ */ e.createElement("path", {
+    d: "M1.85 28.44a4.94 4.94 0 0 0-.15 1.13v66.48a1.94 1.94 0 0 0 .75 1.77l41 23.68 15.09-60.32L1.85 28.44Z",
+    fill: "#7B8AAB"
+  })), h || (h = /* @__PURE__ */ e.createElement("path", {
+    d: "m85.27 98.1-40.35 23.29c-1.21.71-2.2 0-2.2-1.66V53.25a5.27 5.27 0 0 1 2.2-4.2l40.35-23.29c1.21-.7 2.2 0 2.2 1.66V93.9a5.27 5.27 0 0 1-2.2 4.2Z",
+    fill: "#5D6983"
+  })), i || (i = /* @__PURE__ */ e.createElement("path", {
+    fillRule: "evenodd",
+    clipRule: "evenodd",
+    d: "M45.145.56h.005-.025.02Zm0 0a3.06 3.06 0 0 0-1.495.43L3.27 24.29a6.45 6.45 0 0 0-2.82 5.28v66.48a3.13 3.13 0 0 0 1.39 2.86l41 23.67c.363.216.778.33 1.2.33a3.052 3.052 0 0 0 1.5-.43l40.35-23.3a6.45 6.45 0 0 0 2.82-5.28V27.42a3.1 3.1 0 0 0-1.46-2.9c-.34-.22-.86-.53-1.69-1L81 20.88l-14-8.07L46.32.88a2.4 2.4 0 0 0-1.175-.32Zm.545 1.4a1.1 1.1 0 0 0-.57-.15h.03a1.83 1.83 0 0 0-.87.26L3.93 25.37a5.25 5.25 0 0 0-2.2 4.2v66.48a1.94 1.94 0 0 0 .75 1.77l41 23.68c.176.104.376.159.58.16.309-.012.609-.105.87-.27L85.28 98.1a5.27 5.27 0 0 0 2.2-4.2V27.42a1.93 1.93 0 0 0-.79-1.8l-41-23.66Z",
+    fill: "#1A1F25"
+  })), c || (c = /* @__PURE__ */ e.createElement("path", {
+    d: "M45.3 52.83a2.55 2.55 0 0 1 .74-1.66l38.85-22.43v65.58a2.55 2.55 0 0 1-.74 1.66l-38.32 22.13a.35.35 0 0 1-.53-.3V52.83Z",
+    fill: "#1A1F25"
+  })), m || (m = /* @__PURE__ */ e.createElement("path", {
+    d: "M81.09 99.35 54.88 84.21l26.05-15.04 26.21 15.14-26.05 15.04Z",
+    fill: "#3B4356"
+  })), v || (v = /* @__PURE__ */ e.createElement("path", {
+    d: "M47.2 85.87v22.7l21.96-12.68v-22.7L47.2 85.87Z",
+    fill: "#3B4356"
+  })), s || (s = /* @__PURE__ */ e.createElement("path", {
+    clipRule: "evenodd",
+    d: "M47.2 85.87v22.7l21.96-12.68v-22.7L47.2 85.87Z",
+    stroke: "#1A1F25",
+    strokeWidth: 0.66,
+    strokeLinejoin: "round"
+  })), k || (k = /* @__PURE__ */ e.createElement("path", {
+    d: "m69.28 109.06-18.52-10.7 18.41-10.62 18.52 10.69-18.41 10.63Z",
+    fill: "#3B4356"
+  })), u || (u = /* @__PURE__ */ e.createElement("path", {
+    d: "M105.82 83.54 99 79.6l-1.57.51 1.49 8.45s4.66-3.26 6.9-5.02Z",
+    fill: "#5D6983"
+  })), E || (E = /* @__PURE__ */ e.createElement("path", {
+    clipRule: "evenodd",
+    d: "m69.28 109.06-18.52-10.7 18.41-10.62 18.52 10.69-18.41 10.63Z",
+    stroke: "#1A1F25",
+    strokeWidth: 0.66,
+    strokeLinejoin: "round"
+  })), Z || (Z = /* @__PURE__ */ e.createElement("path", {
+    d: "m46.57 115.29 26.78 15.46v-30.64L46.57 84.64v30.65Z",
+    fill: "#7B8AAB"
+  })), _ || (_ = /* @__PURE__ */ e.createElement("path", {
+    d: "m46.57 111.7 26.78 15.47v-8.11l-26.78-15.47v8.11Z",
+    fill: "#7B8AAB"
+  })), f || (f = /* @__PURE__ */ e.createElement("path", {
+    d: "m46.57 106.67 26.78 15.46v-3.07l-26.78-15.47v3.08ZM46.57 87.93l26.78 15.46v-3.28L46.57 84.64v3.29Z",
+    fill: "#A2B5DF"
+  })), L || (L = /* @__PURE__ */ e.createElement("path", {
+    d: "M47.64 112.37v-5.12l4-.71.05-2.57 3.68 2c.07.04.156.039.224-.002a.221.221 0 0 0 .106-.198V93.2h5.7l-14.82-8.56v30.65l26.78 15.46v-3.53l-25.72-14.85Z",
+    fill: "#5D6983"
+  })), A || (A = /* @__PURE__ */ e.createElement("path", {
+    clipRule: "evenodd",
+    d: "m46.57 106.67 26.78 15.46v-3.07l-26.78-15.47v3.08ZM46.57 111.7l26.78 15.47-26.78-15.47Z",
+    stroke: "#3F485E",
+    strokeWidth: 0.66,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  })), B || (B = /* @__PURE__ */ e.createElement("path", {
+    d: "m46.57 87.93 26.78 15.46",
+    stroke: "#3F485E",
+    strokeWidth: 0.66,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  })), M || (M = /* @__PURE__ */ e.createElement("path", {
+    d: "m75.18 67.33 33.14 19.14v-3.1L75.18 64.23v3.1Z",
+    fill: "#A2B5DF"
+  })), D || (D = /* @__PURE__ */ e.createElement("path", {
+    d: "M75.18 67.33 97.47 80.2l2.98-1.38-25.27-14.59v3.1Z",
+    fill: "#5D6983"
+  })), V || (V = /* @__PURE__ */ e.createElement("path", {
+    d: "M68.38 101.87v33.27a1.54 1.54 0 0 0 .62 1.39l4.94 2.85 36.63-26.1 7.11-34.18-4.93-2.85-44.37 25.62Z",
+    fill: "#7B8AAB"
+  })), F || (F = /* @__PURE__ */ e.createElement("path", {
+    d: "m68.38 101.87 5.22 3.02 25.41-11.83 18.67-13.96-4.93-2.85-44.37 25.62Z",
+    fill: "#A2B5DF"
+  })), j || (j = /* @__PURE__ */ e.createElement("path", {
+    clipRule: "evenodd",
+    d: "m68.38 101.87 5.22 3.02 25.41-11.83 18.67-13.96-4.93-2.85-44.37 25.62v0Z",
+    stroke: "#394255",
+    strokeWidth: 0.66,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  })), R || (R = /* @__PURE__ */ e.createElement("path", {
+    d: "m112.75 76.25-44.37 25.62v33.27a2.31 2.31 0 0 0 .62 1.75",
+    stroke: "#394255",
+    strokeWidth: 0.66,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  })), W || (W = /* @__PURE__ */ e.createElement("path", {
+    d: "M73.31 104.72V138c0 1.38 1 1.93 2.18 1.24l40-23.1a4.8 4.8 0 0 0 2.18-3.76V79.1l-44.36 25.62Z",
+    fill: "#5D6983"
+  })), g || (g = /* @__PURE__ */ e.createElement("path", {
+    d: "m117.68 79.1-44.37 25.62V138c0 1.38 1 2.27 2.19 1.57",
+    stroke: "#394255",
+    strokeWidth: 0.66,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  })), w || (w = /* @__PURE__ */ e.createElement("path", {
+    d: "m91 105.1-3.38-1.95v-2.54l10.58-5.26 9.65-1.35v2.54a.731.731 0 0 1-.42.61l-14 8a2.68 2.68 0 0 1-2.43-.05Z",
+    fill: "#7B8AAB"
+  })), b || (b = /* @__PURE__ */ e.createElement("path", {
+    d: "m98.24 95.35-5.75 2.86v7.23c.349.009.693-.074 1-.24l14-8a.731.731 0 0 0 .42-.61V94l-9.67 1.35Z",
+    fill: "#5D6983"
+  })), y || (y = /* @__PURE__ */ e.createElement("path", {
+    d: "m91 102.56-3.38-2 16.21-9.36 3.38 2c.72.42.8 1 .18 1.41l-14 8a2.68 2.68 0 0 1-2.39-.05Z",
+    fill: "#A2B5DF"
+  })), O || (O = /* @__PURE__ */ e.createElement("path", {
+    clipRule: "evenodd",
+    d: "M107.84 94a1 1 0 0 0-.59-.8l-3.38-2-16.21 9.36v2.54l3.34 2c.747.42 1.65.457 2.43.1l14-8a.731.731 0 0 0 .42-.61c0-.33-.01-2.59-.01-2.59Z",
+    stroke: "#394255",
+    strokeWidth: 0.66,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  })), S || (S = /* @__PURE__ */ e.createElement("path", {
+    clipRule: "evenodd",
+    d: "m91 102.56-3.38-2 16.21-9.36 3.38 2c.72.42.8 1 .18 1.41l-14 8a2.68 2.68 0 0 1-2.39-.05v0Z",
+    stroke: "#394255",
+    strokeWidth: 0.66,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  })), x || (x = /* @__PURE__ */ e.createElement("path", {
+    fillRule: "evenodd",
+    clipRule: "evenodd",
+    d: "m74.45 63.21.65-.47.7.41 28.56 16.47 7.75-4.47.62-.36.63.36 4.93 2.85.62.36v34a6 6 0 0 1-2.8 4.84l-40 23.1a3.166 3.166 0 0 1-1.6.47 2.43 2.43 0 0 1-1.19-.31l-4.95-2.86a2.75 2.75 0 0 1-1.24-2.47v-6.54l-21.19-12.23-.62-.36V84l.52-.38 28.61-20.41Zm29.93 17.88-29.2-16.86-28.61 20.41v30.65l21.81 12.59v7.26a1.54 1.54 0 0 0 .62 1.39l4.93 2.85c.173.101.37.153.57.15a2 2 0 0 0 1-.3l40-23.1a4.8 4.8 0 0 0 2.18-3.76V79.1l-4.93-2.85-8.37 4.84Z",
+    fill: "#1A1F25"
+  })), N || (N = /* @__PURE__ */ e.createElement("path", {
+    clipRule: "evenodd",
+    d: "m72.44 99.53 29.25-16.89-29.25 16.89Z",
+    stroke: "#1A1F25",
+    strokeWidth: 0.66,
+    strokeLinejoin: "round"
+  })), P || (P = /* @__PURE__ */ e.createElement("path", {
+    d: "m46.57 84.64 25.87 14.89M101.69 82.64 75.18 67.33",
+    stroke: "#1A1F25",
+    strokeWidth: 0.66,
+    strokeLinejoin: "round"
+  })), q || (q = /* @__PURE__ */ e.createElement("path", {
+    d: "m85.23 25.8-40 23.1A4.82 4.82 0 0 0 43 52.66v33.47L48 89l36-28.56 7.64-32-4.85-2.8a1.55 1.55 0 0 0-1.56.16Z",
+    fill: "#A2B5DF"
+  })), z || (z = /* @__PURE__ */ e.createElement("path", {
+    d: "M43 52.66v33.47L48 89l4.54-3.6 6-24.2-15.49-8.94c.01.12-.05.26-.05.4Z",
+    fill: "#7B8AAB"
+  })), C || (C = /* @__PURE__ */ e.createElement("path", {
+    d: "m90.17 28.64-40 23.1A4.82 4.82 0 0 0 48 55.5V89l44.37-25.62v-33.5c-.03-1.38-1-1.88-2.2-1.24Z",
+    fill: "#5D6983"
+  })), G || (G = /* @__PURE__ */ e.createElement("path", {
+    d: "M92.42 28.62a2.29 2.29 0 0 0-2.25 0l-40 23.1A4.82 4.82 0 0 0 48 55.5V89",
+    stroke: "#394255",
+    strokeWidth: 0.66,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  })), H || (H = /* @__PURE__ */ e.createElement("path", {
+    fillRule: "evenodd",
+    clipRule: "evenodd",
+    d: "M84.58 24.72a3.17 3.17 0 0 1 1.6-.47h.06a2.37 2.37 0 0 1 1.2.32c.217.117 1.695.971 2.966 1.706.899.52 1.694.979 1.864 1.074a2.7 2.7 0 0 1 1.35 2.53v34.2l-.62.36-44.41 25.62-.63.36-.62-.36-4.93-2.85-.63-.36V52.66a6 6 0 0 1 2.8-4.84l40-23.1Zm2.21.93a1.11 1.11 0 0 0-.58-.15h-.03a2.07 2.07 0 0 0-1 .3l-40 23.1A4.82 4.82 0 0 0 43 52.66v33.47L48 89l44.34-25.64V29.88a1.48 1.48 0 0 0-.7-1.43l-4.85-2.8Z",
+    fill: "#1A1F25"
+  })), I || (I = /* @__PURE__ */ e.createElement("path", {
+    d: "m65.68 54.51-3.38-1.95V50l10.58-5.27 9.61-1.35v2.54a.72.72 0 0 1-.42.61l-14 8.06a2.72 2.72 0 0 1-2.39-.08Z",
+    fill: "#7B8AAB"
+  })), J || (J = /* @__PURE__ */ e.createElement("path", {
+    d: "M72.88 44.75 67 47.68v7.17a2.13 2.13 0 0 0 1.1-.24l14-8.06a.72.72 0 0 0 .42-.61V43.4l-9.64 1.35Z",
+    fill: "#5D6983"
+  })), K || (K = /* @__PURE__ */ e.createElement("path", {
+    d: "m65.68 52-3.38-2 16.21-9.36 3.38 2c.72.41.8 1 .18 1.4l-14 8.06a2.72 2.72 0 0 1-2.39-.1Z",
+    fill: "#A2B5DF"
+  })), Q || (Q = /* @__PURE__ */ e.createElement("path", {
+    clipRule: "evenodd",
+    d: "M82.48 43.4a1 1 0 0 0-.59-.79l-3.38-2L62.3 50v2.54l3.38 1.95c.75.414 1.65.45 2.43.1l14-8.06a.72.72 0 0 0 .42-.61c-.04-.25-.05-2.52-.05-2.52Z",
+    stroke: "#394255",
+    strokeWidth: 0.66,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  })), T || (T = /* @__PURE__ */ e.createElement("path", {
+    clipRule: "evenodd",
+    d: "m65.68 52-3.38-2 16.21-9.36 3.38 2c.72.41.8 1 .18 1.4l-14 8.06a2.72 2.72 0 0 1-2.39-.1Z",
+    stroke: "#394255",
+    strokeWidth: 0.66,
+    strokeLinecap: "round",
+    strokeLinejoin: "round"
+  })));
+};
+export {
+  U as default
+};
+//# sourceMappingURL=empty-state-no-data-dark-blue.svg-DQg_Cmcp.mjs.map
---
+++ wwwroot/otto.js
@@ -0,0 +1,17 @@
+﻿// https://git.autodesk.com/forge/otto-chatbot-ui/tree/aec_explorer_integration
+import './dependencies/otto/aec-datamodel-explorer-otto-chat.umd.js'
+const OttoChat = AECDataModelExplorerOttoChat.OttoChat;
+
+window.AECDataModelExplorerOttoChat.getBearerToken = async () => {
+  // There most likely a cleaner way to pass this to the react component
+  // but with the time constraints, this will do for now.
+  const res = await fetch(`api/auth/token`);
+  return res.json();
+}
+
+window.addEventListener("load", async () => {
+  ReactDOM.render(
+    React.createElement(OttoChat),
+    document.getElementById('ottochat'),
+  );
+});
